1f9cd9e6cf2078158bc527f98db5cafb5237ad9f
Caleb Jones <bionicle.dragon@gmail.com>
2014-10-25 21:43:15 -0400

    Add parser and renderer

--- chemset.js
+++ chemset.js
@@ -15,84 +15,315 @@ function Token(name, sub) {
 
 function Compound() {
     this.count = null;
+    this.charge = undefined;
+    this.state = undefined;
     this.tokens = [];
 }
 
+// Build a finite state machine
 var fsm = {
     state: 'outside',
+    tmp: {},
+    equation: [],
     transition: {
         'outside': [
-            { pattern: /[[(A-Z]/, to: 'new token' },
-            { pattern: /\d/, to: 'count compound' }
+            { pattern: /[[(A-Z]/, to: 'new_token' },
+            { pattern: /\d/, to: 'count_compound' },
+            { pattern: /\s/, to: 'outside' },
+            { pattern: /\+/, to: 'outside' },
+            { pattern: /-/, to: 'becomes' },
+            { pattern: /\0/, to: 'outside' }
         ],
-        'count compound': [
-            { pattern: /[[(A-Z]/, to: 'new token' },
-            { pattern: /\d/, to: 'count compound' }
+        'count_compound': [
+            { pattern: /[[(A-Z]/, to: 'new_token' },
+            { pattern: /\d/, to: 'count_compound' }
         ],
-        'new token': [
-            { pattern: /[()A-Z[\]]/, to: 'new token' },
-            { pattern: /[a-z]/, to: 'continue token' },
-            { pattern: /\d/, to: 'count token' },
+        'new_token': [
+            { pattern: /[()A-Z[\]]/, to: 'new_token' },
+            { pattern: /[a-z]/, to: 'continue_token' },
+            { pattern: /\d/, to: 'count_token' },
             { pattern: /[+-]/, to: 'charge' },
-            { pattern: /\s/, to: 'charge wait' }
+            { pattern: /\s/, to: 'charge_wait' },
+            { pattern: /\0/, to: 'outside' }
         ],
-        'continue token': [
-            { pattern: /[()A-Z[\]]/, to: 'new token' },
-            { pattern: /[a-z]/, to: 'continue token' },
-            { pattern: /\d/, to: 'count token' },
+        'continue_token': [
+            { pattern: /[()A-Z[\]]/, to: 'new_token' },
+            { pattern: /[a-z]/, to: 'continue_token' },
+            { pattern: /\d/, to: 'count_token' },
             { pattern: /[+-]/, to: 'charge' },
-            { pattern: /\s/, to: 'charge wait' }
+            { pattern: /\s/, to: 'charge_wait' },
+            { pattern: /\0/, to: 'outside' }
         ],
-        'count token': [
-            { pattern: /[()A-Z[\]]/, to: 'new token' },
-            { pattern: /\d/, to: 'count token' },
+        'count_token': [
+            { pattern: /[()A-Z[\]]/, to: 'new_token' },
+            { pattern: /\d/, to: 'count_token' },
             { pattern: /[+-]/, to: 'charge' },
-            { pattern: /\s/, to: 'charge wait' }
+            { pattern: /\s/, to: 'charge_wait' },
+            { pattern: /\0/, to: 'outside' }
         ],
         'charge': [
-            { pattern: /\s/, to: 'state wait' }
+            { pattern: /\s/, to: 'state_wait' },
+            { pattern: /\0/, to: 'outside' }
         ],
-        'charge wait': [
-            { pattern: /\s/, to: 'charge wait' },
-            { pattern: /\d/, to: 'charge count' },
+        'charge_wait': [
+            { pattern: /\s/, to: 'charge_wait' },
+            { pattern: /\d/, to: 'charge_count' },
             { pattern: /\(/, to: 'state' },
-            { pattern: /\+/, to: 'outside' }
+            { pattern: /\+/, to: 'outside' },
+            { pattern: /\0/, to: 'outside' }
         ],
-        'charge count': [
-            { pattern: /\d/, to: 'charge count' },
-            { pattern: /[+-]/, to: 'charge end' }
+        'charge_count': [
+            { pattern: /\d/, to: 'charge_count' },
+            { pattern: /[+-]/, to: 'charge_end' }
         ],
-        'charge end': [
-            { pattern: /\s/, to: 'state wait' }
+        'charge_end': [
+            { pattern: /\s/, to: 'state_wait' },
+            { pattern: /\0/, to: 'outside' }
         ],
-        'state wait': [
+        'state_wait': [
             { pattern: /\(/, to: 'state' },
-            { pattern: /\+/, to: 'outside' },    
+            { pattern: /\+/, to: 'outside' },
+            { pattern: /\0/, to: 'outside' }
         ],
         'state': [
             { pattern: /(a|q|s|l)/, to: 'state' },
-            { pattern: /\)/, to: outside }
-        ]
+            { pattern: /\)/, to: 'outside' }
+        ],
+        'becomes': [
+            { pattern: /-/, to: 'becomes' },
+            { pattern: />/, to: 'outside' }
+        ],
+        'error': []
+    },
+    act: function (character) {
+        var tran = this.transition[this.state];
+        var l = tran.length;
+        var next_state = 'error';
+        for (var i = 0; i < l; i++) {
+            var obj = tran[i];
+            if (obj.pattern.test(character)) {
+                next_state = obj.to;
+                break;
+            }
+        }
+        // console.log(this.state+" -> "+next_state+" ("+character+")");
+        this.state = next_state;
+        return this.state;
     }
 }
 
+// Parse the string and turn it into a token stream
 function parse(text) {
-    var equation = [];
+    // Add a null terminator for the FSM to check
+    var text = text + "\0"
     var comp = undefined;
-    var current_token = undefined;
     var number = /\d/;
     var whitespace = /\s/;
-    var count = "";
+    // Initialize the FSM
+    fsm.state = 'outside';
+    fsm.equation = [];
+    fsm.tmp = {};
+    // Iterate over the string and give each
+    // character to the FSM
     for (var i = 0; i < text.length; i++) {
-        var symbol = text[i];
-        equation.push(symbol);
+        var character = text[i];
+        var state1 = fsm.state;
+        var state2 = fsm.act(character);
+
+        // Fail on error
+        if (state2 === 'error') {
+            return "ERROR at character " + i;
+        }
+        // Leaving 'outside'
+        if (state1 === 'outside' &&
+            state2 !== 'outside') {
+            comp = new Compound();
+            if (state2 !== 'count_compound') {
+                comp.count = undefined;
+            }
+        }
+        if (state2 === 'count_compound') {
+            // Entering count_compound
+            if (state1 === 'outside') {
+                fsm.tmp.count = character;
+            } else {
+                fsm.tmp.count += character;
+            }   
+        }
+       // Add the digit into the token count
+        if (state2 === 'count_token') {
+            if (state1 !== 'count_token') {
+                fsm.tmp.count_string = character;
+            } else {
+                fsm.tmp.count_string += character;
+            }
+        }
+        // Leaving 'count_token'
+        // Save the count into the token
+        if (state1 === 'count_token' &&
+            state2 !== 'count_token') {
+            fsm.tmp.count = parseInt(fsm.tmp.count_string);
+        }
+        // Leaving 'count_compound'
+        if (state1 === 'count_compound' &&
+            state2 !== 'count_compound') {
+            comp.count = parseInt(fsm.tmp.count);
+        }
+        // Entering 'outside'
+        if (state2 === 'outside' &&
+            state1 !== 'outside') {
+            if (comp.tokens.length > 0) {
+                // If they haven't been set, they'll just
+                // set it to undefined again, so no harm
+                comp.charge = fsm.tmp.charge;
+                fsm.tmp.charge = undefined;
+                comp.state = fsm.tmp.state;
+                fsm.tmp.state = undefined;
+                
+                // console.log(comp)
+                fsm.equation.push(comp);
+            }
+        }
+        // Save a + or -
+        if (state2 === 'charge') {
+            fsm.tmp.charge = character;
+        }
+        if (state2 === 'charge_count' ||
+            state2 === 'charge_end') {
+            if (state1 === 'charge_wait') {
+                fsm.tmp.charge = character;
+            } else {
+                fsm.tmp.charge += character;
+            }
+        }
+        if (state2 === 'state') {
+            if (state1 !== 'state') {
+                fsm.tmp.state = "";
+            } else {
+                if (character !== ")") {
+                    fsm.tmp.state += character;
+                }
+            }
+        }
+        // Entering 'becomes' (the -->)
+        if (state2 === 'becomes' &&
+            state1 !== 'becomes') {
+            fsm.equation.push("-->");
+        }
+
+        // Time to save any potential temporary tokens
+        if (state2 === 'new_token' ||
+            state2 === 'charge' ||
+            state2 === 'charge_wait' ||
+            state2 === 'outside') {
+            if (fsm.tmp.token_string !== undefined) {
+                var token = new Token(fsm.tmp.token_string,
+                                       fsm.tmp.count);
+                comp.tokens.push(token);
+                // console.log(token);
+                fsm.tmp.token_string = undefined;
+                fsm.tmp.count = undefined;
+            }
+        }
+
+        if (state2 === 'new_token') {
+            fsm.tmp.token_string = character;
+        }
+        if (state2 === 'continue_token') {
+            fsm.tmp.token_string += character;
+        }
     }
-    console.log(equation);
+    return fsm.equation;
 }
 
+// Run a function for every element in the page
+// that has a given CSS selector
 forEachElement("p", function (el, i) {
+    // Remove ' + ' from a string
+    var stripPlus = function (string) {
+        return string.substring(0, string.length - 3);
+    };
     var list = el.innerText.match(chemRegex);
-    for (var i = 0; i < list.length; i++) {
-        parse(list[i]);
+    var l = list.length;
+    for (var i = 0; i < l; i++) {
+        if (list[i].length > 1) {
+            var result = parse(list[i]);
+            // If the result contains anything
+            if (result.length > 0) {
+                res_string = '<span class="chemset">';
+                var l2 = result.length;
+                for (var j = 0; j < l2; j++) {
+                    var compound = result[j];
+                    if (compound === '-->') {
+                        res_string = stripPlus(res_string);
+                        res_string += ' <span class="chem-arrow"><span ' +
+                            'class="chem-hidden">--></span></span> ';
+                    } else {
+                        var l3 = compound.tokens.length;
+                        for (var t = 0; t < l3; t++) {
+                            var token = compound.tokens[t];
+                            res_string += token.string
+                            if (token.subscript) {
+                                res_string += '<span class="chem-sub">' +
+                                    token.subscript + '</span>';
+                            }
+                        }
+                        if (compound.charge) {
+                            if (compound.charge.length > 1) {
+                                // Add an invisible space if it's n+ or n-
+                                // This helps reproduce the original input
+                                res_string += '<span class="chem-hidden"> ' +
+                                    '</span>';
+                            }
+                            compound.charge = compound.charge.replace("-",
+                                '<span class="chem-hidden">-</span>' +
+                                '<span class="chem-negative"></span>')
+                                .replace("+",'<span class="chem-hidden">' +
+                                    '+</span><span class="chem-positive">' +
+                                    '</span>')
+                            res_string += '<span class="chem-charge">' +
+                                compound.charge  + '</span>';
+                        }
+                        if (compound.state) {
+                            res_string += '<span class="chem-hidden"> ' +
+                                    '</span><span class="chem-state">(<i>' +
+                                compound.state + '</i>)</span>';
+                        }
+                        res_string += " + "
+                    }
+                }
+                res_string = stripPlus(res_string);
+                res_string += '</span>';
+                console.log(res_string);
+                // console.log(el.innerText.replace(list[i],
+                //                                  res_string));
+                el.innerHTML = el.innerHTML.replace(htmlEscape(list[i]),
+                                                      res_string);
+
+            }
+            // console.log(result);
+        }
     }
 });
+
+function htmlEscape(str) {
+    return String(str)
+            .replace(/&/g, '&amp;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;');
+}
+
+
+var style = document.createElement('style')
+style.type = 'text/css'
+style.innerHTML = '.chem-hidden { font-size: 0; }\n' +
+'.chem-negative:before { font-weight: bold; content: "\u2013"; }\n' +
+'.chem-positive:before { font-weight: bold; content: "+"; }\n' +
+'.chem-arrow:before { content: "\u27F6"; }\n' +
+'.chem-state, .chem-charge, .chem-sub { font-size: 75%; ' +
+'line-height: 0; position: relative; vertical-align: baseline; } \n' +
+'.chem-state, .chem-sub { bottom: -0.25em; }\n' +
+'.chem-charge { top: -0.5em; }';
+document.getElementsByTagName('head')[0].appendChild(style)
